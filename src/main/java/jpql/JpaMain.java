5005080a554eae82920a51b5142fabd667538861
minho0315 <dlalsgh369@naver.com>
2022-06-12 15:46:02 +0900

    페치 조인 1 - 기본

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,22 +15,40 @@ public class JpaMain {
 
         try {
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.team From Member m";
+            String query = "select distinct t From Team t join fetch t.members";
             List<Team> resultList = em.createQuery(query, Team.class).getResultList();
 
-            for (Team s : resultList) {
-                System.out.println("s = " + s);
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + "|team=  " + team.getMembers().size());
+                for ( Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
 
             tx.commit();
